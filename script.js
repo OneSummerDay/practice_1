7b68ed1a2fbb1cf0998e832ea347e75ddde11773
Artem Kraievyi <artem.kraievyi@gmail.com>
2024-10-15 22:32:40 +0300

    refactor: rewritten prompts in a loop and added movie count check

--- script.js
+++ script.js
@@ -8,12 +8,37 @@ const personalMoviesDB = {
   private: false,
 }
 
-const a = promt('What is the last one?', '')
-const b = promt('What rating would you give?', '')
-const c = promt('What is the last one?', '')
-const d = promt('What rating would you give?', '')
+// const a = promt('What is the last one?', '')
+// const b = promt('What rating would you give?', '')
+// const c = promt('What is the last one?', '')
+// const d = promt('What rating would you give?', '')
 
-personalMoviesDB.movies[a] = b
-personalMoviesDB.movies[c] = d
+// personalMoviesDB.movies[a] = b
+// personalMoviesDB.movies[c] = d
 
+for (let i = 0; i < 2; i++) {
+  const a = promt('What is the last one?', '')
+  const b = promt('What rating would you give?', '')
+
+  if (
+    a != null &&
+    b != null &&
+    a != '' &&
+    b != '' &&
+    a.length < 50 &&
+    b.length < 50
+  ) {
+    personalMoviesDB.movies[a] = b
+  } else {
+    i--
+  }
+}
+
+if (personalMoviesDB.count <= 10) {
+  console.log('I`ve seen a few films! ')
+} else if (30 >= personalMoviesDB.count >= 10) {
+  console.log('You have seen sufficient quantity of films!')
+} else {
+  console.log(' You have you seen a lot of films')
+}
 console.log(personalMoviesDB)
